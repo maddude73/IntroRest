00c7809778d2c477df96670caaa959642434f3dc
Simon Roberts <simon@dancingcloudservices.com>
2018-02-28 14:42:18 -0500

    Example with per-invocation injection of header params

--- src/main/java/com/example/CustomersRootResource.java
+++ src/main/java/com/example/CustomersRootResource.java
@@ -1,22 +1,27 @@
 package com.example;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.HashMap;
 import java.util.Map;
 
 @Path("/customers")
 public class CustomersRootResource {
+  @DefaultValue("not defined") @HeaderParam("Simon-One") private String s1;
+  @DefaultValue("not defined") @HeaderParam("Simon-Two") private String s2;
   private static final Map<Integer, String> MY_TABLE = new HashMap<>();
   static {
     MY_TABLE.put(1, "Fred, our first customer");
     MY_TABLE.put(3, "Jim, a loyal customer");
     MY_TABLE.put(92, "Sheila, buys lots of stuff");
-    MY_TABLE.put(1, "Alan, nuisance, always returns stuff");
-    MY_TABLE.put(1, "Mary, brings her friends to the store");
+    MY_TABLE.put(7, "Alan, nuisance, always returns stuff");
+    MY_TABLE.put(16, "Mary, brings her friends to the store");
+  }
+  @GET
+  @Path("/one/{path:.*}")
+  @Produces(MediaType.TEXT_PLAIN)
+  public String getSubStuff(@PathParam("path") String path) {
+    return "This is funky path: " + path;
   }
   @GET
   @Path("/one")
@@ -35,10 +40,11 @@ public class CustomersRootResource {
   @Produces(MediaType.TEXT_PLAIN)
   public String getCustByPk(@PathParam("pk") int id) {
     String custInfo = MY_TABLE.get(id);
+    String headerInfo = "header info is " + s1 + " " + s2;
     if (custInfo != null) {
-      return custInfo;
+      return custInfo + headerInfo;
     } else {
-      return "Er, who? I don't know customer id " + id;
+      return "Er, who? I don't know customer id " + id + headerInfo;
     }
   }
 }
