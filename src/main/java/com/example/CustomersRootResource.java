fd0b14b5592b493105cb4e41a7ef4b6f7f8bc549
Simon Roberts <simon@dancingcloudservices.com>
2018-02-28 14:04:47 -0500

    PathParameter injection

--- src/main/java/com/example/CustomersRootResource.java
+++ src/main/java/com/example/CustomersRootResource.java
@@ -2,6 +2,7 @@ package com.example;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -18,5 +19,11 @@ public class CustomersRootResource {
   @Produces(MediaType.TEXT_PLAIN)
   public String getCustTwo() {
     return "This is customer two, name is Jim";
+  }
+  @GET
+  @Path("/{pk: \\d+}")
+  @Produces(MediaType.TEXT_PLAIN)
+  public String getCustByPk(@PathParam("pk") int id) {
+    return "This is customer two, name is something else, Primary key requested is " + id;
   }
 }
