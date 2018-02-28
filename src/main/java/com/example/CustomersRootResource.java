81b05f60986f9d703daa7d76d9e9e56674df84a8
Simon Roberts <simon@dancingcloudservices.com>
2018-02-28 14:11:34 -0500

    Illustrating using a Map in Java (configured as static)

--- src/main/java/com/example/CustomersRootResource.java
+++ src/main/java/com/example/CustomersRootResource.java
@@ -5,9 +5,19 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+import java.util.Map;
 
 @Path("/customers")
 public class CustomersRootResource {
+  private static final Map<Integer, String> MY_TABLE = new HashMap<>();
+  static {
+    MY_TABLE.put(1, "Fred, our first customer");
+    MY_TABLE.put(3, "Jim, a loyal customer");
+    MY_TABLE.put(92, "Sheila, buys lots of stuff");
+    MY_TABLE.put(1, "Alan, nuisance, always returns stuff");
+    MY_TABLE.put(1, "Mary, brings her friends to the store");
+  }
   @GET
   @Path("/one")
   @Produces(MediaType.TEXT_PLAIN)
@@ -24,6 +34,11 @@ public class CustomersRootResource {
   @Path("/{pk: \\d+}")
   @Produces(MediaType.TEXT_PLAIN)
   public String getCustByPk(@PathParam("pk") int id) {
-    return "This is customer two, name is something else, Primary key requested is " + id;
+    String custInfo = MY_TABLE.get(id);
+    if (custInfo != null) {
+      return custInfo;
+    } else {
+      return "Er, who? I don't know customer id " + id;
+    }
   }
 }
